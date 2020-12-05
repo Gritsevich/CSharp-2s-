eb2b2196703512862a87c541c5c8832c396ac570
Gritsevich <62441416+Gritsevich@users.noreply.github.com>
2020-12-05 14:28:36 +0300

    Create ConfigManager.cs

---
+++ 3L/ConfigManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class ConfigManager : IParser
+    {
+        readonly IParser parser;
+        public ConfigManager(string path, string config)
+        {
+            if (Path.GetExtension(path) == ".xml")
+            {
+                parser = new XmlParse(path, config);
+            }
+           else parser = new JsonPars(path);
+        }
+        public Opt ParseOptions() => parser.ParseOptions();
+        public Opt ParsersOptions()
+        {
+            throw new NotImplementedException();
+        }
+    }
